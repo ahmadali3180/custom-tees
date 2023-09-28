5adb9ebe4d3d063d0d68938258bc9af841209cae
ahmadali3180 <mahmadaliwork3180@gmail.com>
2023-09-29 02:49:12 +0500

    client complete

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import CustomButton from "../components/CustomButton"
 
-const AIPicker = () => {
+const AIPicker = ({prompt, setPrompt, generatingImg, handleSubmit}) => {
   return (
-    <div>AIPicker</div>
+    <div className='aipicker-container'>
+      <textarea
+        className='aipicker-textarea rounded-[6px] text-white'
+        placeholder='Tell AI your design...'
+        rows={5}
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+      <div className='flex flex-wrap gap-3'>
+        {generatingImg ? 
+        (
+          <CustomButton
+            type="outline"
+            title="AI is designing"
+            customStyles="text-xs flex-[0.45] w-full items-center justify-center px-1 py-1"
+          />
+        ) : 
+        (
+          <>
+            <CustomButton
+              type="outline"
+              title="AI Logo"
+              handleClick={() => handleSubmit("logo")}
+              customStyles="text-xs flex-[0.45] w-full items-center justify-center px-1 py-1"
+            />
+            <CustomButton
+              type="filled"
+              title="AI Full"
+              handleClick={() => handleSubmit("full")}
+              customStyles="text-xs flex-[0.45] w-full items-center justify-center px-1 py-1"
+            />
+          </>
+        )}
+       
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -97,6 +97,19 @@ const Customizer = () => {
     if(!prompt) return alert("Please enter a prompt.")
 
     try {
+      setGeneratingImg(true)
+      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        method: "POST",
+        headers: {
+          "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({
+          prompt,
+        })
+      })
+
+      const data = await response.json()
+      handleDecals(type, `data:img/png;base64,${data.photo}`)
 
     } catch (error) {
        alert(error)
