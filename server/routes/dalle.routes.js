864f3c13198f2d8210cc926de3bf7ebbe186c71d
ahmadali3180 <mahmadaliwork3180@gmail.com>
2023-09-29 03:03:27 +0500

    core.crlf git err fix

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,36 @@
+import express from "express"
+import * as dotenv from "dotenv"
+import {OpenAI} from "openai"
+
+dotenv.config()
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+const router = express.Router()
+
+router.route("/").get((req, res) => {
+    res.status(200).json({messgae: "Hello for DALL.E Routes"})
+})
+
+router.route("/").post(async (req, res) => {
+    try {
+        const { prompt } = req.body
+
+        const response = await openai.images.generate({
+            prompt,
+            n: 1, 
+            size: "1024x1024",
+            response_format: "b64_json"
+        })
+        
+        const image = response.data[0].b64_json
+        res.status(200).json({photo: image})
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Something went wrong"})
+    }
+})
+
+export default router
